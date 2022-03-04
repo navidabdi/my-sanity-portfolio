b238dda4da19db5ec7aa3d9840c0bb8e53c184bb
navidabdi <artsanaat@gmail.com>
2022-03-04 20:53:41 +0330

    make the social media icons position fixed

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-import { Navbar } from './components';
+import { Navbar, SocialMedia } from './components';
 import './App.scss';
 
 const App = () => (
@@ -13,6 +13,7 @@ const App = () => (
     <Skills />
     <Testimonial />
     <Footer />
+    <SocialMedia />
   </div>
 );
 
--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsGithub, BsInstagram, BsLinkedin, BsGlobe } from 'react-icons/bs';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
-      <BsTwitter />
-    </div>
-    <div>
-      <FaFacebookF />
-    </div>
-    <div>
+    <a target="_blank" href="https://github.com/navidabdi">
+      <BsGithub />
+    </a>
+    <a target="_blank" href="https://www.linkedin.com/in/nabiabdi/">
+      <BsLinkedin />
+    </a>
+    <a target="_blank" href="https://instagram.com/nabiabdii">
       <BsInstagram />
-    </div>
+    </a>
+    <a target="_blank" href="https://webkima.com/">
+      <BsGlobe />
+    </a>
   </div>
 );
 
