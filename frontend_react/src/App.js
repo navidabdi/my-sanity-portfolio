62ea6f05361ca1d986d5858c4217f85fcb6f7b25
navidabdi <artsanaat@gmail.com>
2022-03-04 13:58:56 +0330

    add components

---
+++ frontend_react/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+// import { Navbar } from './components';
+import './App.scss';
+
+const App = () => (
+  <div className="app">
+    <h1>App</h1>
+    {/* <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer /> */}
+  </div>
+);
+
+export default App;
