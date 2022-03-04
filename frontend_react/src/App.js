18a76d0fd237d3d032fd9fc364fcc65f98ba6407
navidabdi <artsanaat@gmail.com>
2022-03-04 14:07:23 +0330

    Add assets

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-// import { Navbar } from './components';
+import { Navbar } from './components';
 import './App.scss';
 
 const App = () => (
   <div className="app">
-    <h1>App</h1>
-    {/* <Navbar />
+    <Navbar />
     <Header />
     <About />
     <Work />
     <Skills />
     <Testimonial />
-    <Footer /> */}
+    <Footer />
   </div>
 );
 
