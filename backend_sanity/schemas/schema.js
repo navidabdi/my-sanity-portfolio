85cce5a05d275c8104c423e18e285d57a6c6d927
navidabdi <artsanaat@gmail.com>
2022-03-04 12:35:11 +0330

    add workExperience schema

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -9,6 +9,8 @@ import brands from './brands';
 import contact from './contact';
 import experiences from './experiences';
 import skills from './skills';
+import workExperience from './workExperience';
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -22,5 +24,6 @@ export default createSchema({
     contact,
     experiences,
     skills,
+    workExperience,
   ]),
 });
